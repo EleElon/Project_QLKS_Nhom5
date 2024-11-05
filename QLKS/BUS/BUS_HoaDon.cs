8543fcc3ebf31c16d231262de29a45cb1a7cd3a9
Nvthan2004 <nguyenvanthan14022004@gmail.com>
2024-11-05 15:58:36 +0700

    Update Hoa don va frm csvc

--- QLKS/BUS/BUS_HoaDon.cs
+++ QLKS/BUS/BUS_HoaDon.cs
@@ -10,14 +10,36 @@ namespace BUS
 {
     public class BUS_HoaDon
     {
-        DAO_HoaDon dao_HoaDon = new DAO_HoaDon();
+        DAO_HoaDon chiTietHoaDonDAO = new DAO_HoaDon();
+       
         public List<ChiTietHoaDon> HienThi()
         {
-            return dao_HoaDon.Xem();
+            return chiTietHoaDonDAO.Xem();  
         }
         public void LoadDSDV(ComboBox cb)
         {
             DAO_DanhSachDichVu.Instance.LoadComBoBoxDatPhong(cb);
+        }
+        public bool ThemChiTietHoaDon(ChiTietHoaDon chiTietHoaDon)
+        {
+            return chiTietHoaDonDAO.ThemChiTietHoaDon(chiTietHoaDon);
+        }
+
+        // Gọi hàm xóa chi tiết hóa đơn
+        public bool XoaChiTietHoaDon(string maChiTietHoaDon)
+        {
+            return chiTietHoaDonDAO.XoaChiTietHoaDon(maChiTietHoaDon);
+        }
+
+        // Gọi hàm sửa chi tiết hóa đơn
+        public bool SuaChiTietHoaDon(ChiTietHoaDon chiTietHoaDonCapNhat)
+        {
+            return chiTietHoaDonDAO.SuaChiTietHoaDon(chiTietHoaDonCapNhat);
+        }
+        public List<ChiTietHoaDon> TimKiemHoaDonTheoMa(string maHoaDon)
+        {
+            // Gọi hàm từ lớp DAO để tìm kiếm hóa đơn theo mã
+            return chiTietHoaDonDAO.TimKiemHoaDonTheoMa(maHoaDon);
         }
     }
 }
